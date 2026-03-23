476098081e63662681711c571c0ac6caa4f8f56c
Seungwon <zachary@pospro.co.kr>
2026-03-23 17:18:20 +0900

    바이브 코딩 환경 구축 완료 미래전자 화이팅

--- hello.py
+++ hello.py
@@ -3,4 +3,5 @@ from datetime import datetime
 print("안녕하세요, King입니다!")
 print(f"현재 날짜와 시간: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 print("정승원은 King이야. 내가 제일 잘나가~~~")
-
+print("미래전자 ODT 최고!")
+print("바이브 코딩 환경 구축 완료!")
